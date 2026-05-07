583d2f699c5856b2edaf9275fe1b2eba1e1ffd0e
unknown <arnab0574@gmail.com>
2026-05-07 21:10:09 +0600

    Simple Flask server in python(Edited)

--- app1_hello/python/app.py
+++ app1_hello/python/app.py
@@ -1,14 +1,18 @@
-# This is a simple Flask application that will show "Hello, World!", Port and Container id.  
-
-from flask import Flask
-import os
+from flask import Flask, jsonify
+import os, socket
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World! This is Port: ' + os.getenv('PORT') + ' and Container ID: ' + os.getenv('HOSTNAME')  
- 
-if __name__ == '__main__':    
-    app.run(host='0.0.0.0', port=3000)
+ENV = os.getenv("ENV_VALUE", "No env set")
+HOSTNAME = socket.gethostname()
 
+@app.get("/")
+def hello():
+    return jsonify({
+        "message": "Hello from Simple App (Python Flask)",
+        "env": ENV,
+        "container": HOSTNAME
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
